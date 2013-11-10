06c739c531089a0e0228a40a2ba32d9a92b2a4f5
ErikS <erikskoglund88@gmail.com>
2013-11-10 18:27:13 +0100

    added middleclass submodule

--- main.lua
+++ main.lua
@@ -1,21 +1,2 @@
-class = require 'lib/middleclass'
+require 'karlek'
 
-function love.load()
-	love.graphics.setColor(0,0,0,255)
-	love.graphics.setBackgroundColor(255,255,255)
-
-end
-
-function love.keypressed(key, code)
-	if key == 'escape' then
-		love.event.push("quit")
-	end
-end
-
-function love.update(dt)
-	local delta = dt * 1000.0
-
-end
-
-function love.draw()
-end
